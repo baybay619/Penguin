9a5b3c23dc6ef0643a30ba814ae2bda8fe1ebc9a
Arthur <imacatboy@gmail.com>
2014-02-23 19:17:41 -0800

    Forgot to add some methods
    Added getPlayer, getPlayerByName, findBuddy and update functions for
    player clothing (updateColor, updateFlag, updatePhoto, etc).

--- Penguin/PenguinInterface.php
+++ Penguin/PenguinInterface.php
@@ -2,9 +2,24 @@
 
 interface PenguinInterface {
 	
+	public function getPlayer($intPlayer);
+	public function getPlayerByName($strUsername);
+	
+	public function findBuddy($intPlayer);
 	public function joinRoom($intRoom, $intX = 0, $intY = 0);
+	
 	public function sendMessage($strMessage);
 	public function sendPosition($intX, $intY);
+	
+	public function updateColor($intColor);
+	public function updateHead($intItem);
+	public function updateFace($intItem);
+	public function updateNeck($intItem);
+	public function updateBody($intItem);
+	public function updateHand($intItem);
+	public function updateFeet($intItem);
+	public function updateFlag($intItem);
+	public function updatePhoto($intItem);
 	
 }
 
